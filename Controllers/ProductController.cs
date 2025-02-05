9539cda198579901f77cd1e723136064cb164d6a
Annoh Prince Philip Lartey <Annohprince810@gmail.com>
2025-02-05 23:43:34 +0000

    Create ProductController.cs

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly InventoryDbContext _context;
+
+    public ProductController(InventoryDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> AddProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetProducts), new { id = product.Id }, product);
+    }
+}
